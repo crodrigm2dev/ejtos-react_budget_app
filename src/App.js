021b1e4732bf2432da36b45164a5abb3d2d82fb4
crodrigm2dev <167351417+crodrigm2dev@users.noreply.github.com>
2024-05-31 11:08:01 -0500

    Update App.js expenses total
    Import expense total

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Budget from './components/Budget';
 
 // Add code to import the other components here under
 import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
